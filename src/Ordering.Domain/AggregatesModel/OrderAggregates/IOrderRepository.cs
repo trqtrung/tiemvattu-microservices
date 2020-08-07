a8f5f067dc7ae80135716d0cd7974cad210c518a
Tran Quoc Trung <tranquoctrung.1610@gmail.com>
2020-08-07 21:39:42 +0700

    add entity

---
+++ src/Ordering.Domain/AggregatesModel/OrderAggregates/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.AggregatesModel.OrderAggregates
+{
+    public interface IOrderRepository
+    {
+        Order Add(Order order);
+
+        void Update(Order order);
+
+        Task<Order> GetAsync(int orderId);
+    }
+}
---
+++ src/Ordering.Infrastructure/OrderingContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Ordering.Infrastructure
+{
+    public class OrderingContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+        public DbSet<Order> Orders { get; set; }
+    }
+}
