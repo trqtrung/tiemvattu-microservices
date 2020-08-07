cd5674d02f73b861f78df8561baa004ce89343fc
Tran Quoc Trung <tranquoctrung.1610@gmail.com>
2020-08-07 16:43:39 +0700

    init commit start with ordering service first

---
+++ src/Ordering.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ordering.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/tiemvattu-microservices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tiemvattu_microservices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
